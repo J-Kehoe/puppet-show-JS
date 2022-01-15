de049004a06988931dcf9de0d709f5687fdb9d4f
J-Kehoe <jkkehoe177@gmail.com>
2022-01-15 23:33:44 +1100

    init repo

---
+++ src/models/stage.js
@@ -0,0 +1,65 @@
+import * as THREE from '../../node_modules/three/src/Three.js';
+
+const loader = new THREE.TextureLoader()
+
+const cubetexture = loader.load("/wood.jpeg")
+cubetexture.wrapS = THREE.RepeatWrapping;
+cubetexture.wrapT = THREE.RepeatWrapping;
+cubetexture.repeat.set( 15, 5 );
+
+const curtaintexture = loader.load("/curtain.jpg")
+curtaintexture.wrapS = THREE.RepeatWrapping;
+curtaintexture.wrapT = THREE.RepeatWrapping;
+curtaintexture.repeat.set( 1, 5 );
+
+const cubegeometry = new THREE.BoxGeometry(50, 5, 20)
+const cubematerial = new THREE.MeshPhongMaterial({ map: cubetexture, wireframe: false})
+const cube = new THREE.Mesh(cubegeometry, cubematerial)
+cube.receiveShadow = true;
+cube.position.set(0, 2.5, -5);
+cube.name = "cube";
+
+let video = document.getElementById( 'video' );
+
+const videoTexture = new THREE.VideoTexture( video );
+videoTexture.wrapS = THREE.RepeatWrapping;
+videoTexture.repeat.x = - 1;
+if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
+    const constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
+    navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
+        // apply the stream to the video element used in the texture
+        video.srcObject = stream;
+        video.play();
+        console.log("video initialized");
+    } ).catch( function ( error ) {
+        console.error( 'Unable to access the camera/webcam.', error );
+    } );
+} else {
+    console.error( 'MediaDevices interface not available.' );
+}
+
+const planegeometry = new THREE.PlaneGeometry( 12.8*2, 7.2*2, 1 );
+const planematerial = new THREE.MeshStandardMaterial( {map: videoTexture} );
+const plane = new THREE.Mesh( planegeometry, planematerial );
+plane.position.set(0, 14, -12)
+plane.name = "screen";
+plane.receiveShadow = true;
+
+const curtaingeometry = new THREE.PlaneGeometry( 10, 40, 1 );
+const curtainmaterial = new THREE.MeshStandardMaterial( {map: curtaintexture} );
+const curtain = new THREE.Mesh( curtaingeometry, curtainmaterial );
+const curtain2 = new THREE.Mesh( curtaingeometry, curtainmaterial );
+curtain.receiveShadow = true;
+curtain2.receiveShadow = true;
+curtain.position.set(-20, 10, -8)
+curtain2.position.set(20, 10, -8)
+
+const backWallGeo = new THREE.PlaneGeometry(40, 20, 1)
+const backWallMat = new THREE.MeshBasicMaterial({color: 0x000000})
+const backWall = new THREE.Mesh(backWallGeo, backWallMat)
+backWall.position.set(0, 15, -15)
+
+const stage = new THREE.Object3D();
+stage.add(cube, plane, curtain, curtain2, backWall);
+
+export default stage;
---
+++ src/script.js
@@ -0,0 +1,128 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import stage from '../src/models/stage.js'
+import hand from '../src/models/hand.js'
+import { setUpHandTracking } from './utils/handUtils';
+
+
+
+
+/** 
+ * Debug
+ */
+const gui = new dat.GUI()
+
+/**
+ * Constants
+ */
+
+const videoElement = document.getElementById('video');
+const NumberOfHands = 2
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/** 
+ * Objects 
+ * */
+
+scene.add(stage)
+stage.position.set(0, -15, -30)
+
+var hands = new THREE.Object3D()
+for (let i = 0; i < NumberOfHands; i++) {
+    hands.add(hand.clone())
+}
+
+scene.add(hands)
+setUpHandTracking(hands, videoElement)
+
+//#region Lights
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.set(0, 20, 5)
+scene.add(pointLight)
+
+//#endregion Lights
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor("#4a4e69")
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.y = -4
+camera.position.z = -11
+camera.rotation.x += -Math.PI*0.01
+
+scene.add(camera)
+
+// Debug Camera
+const debugCamera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000)
+debugCamera.position.y = 0.5
+debugCamera.position.z = 5
+
+const debugControls = new OrbitControls( debugCamera, renderer.domElement );
+scene.add(debugCamera)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    debugControls.update()
+    const elapsedTime = clock.getElapsedTime()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
