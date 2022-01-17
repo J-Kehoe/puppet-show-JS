fd187f07e0dc66d053ddf86c86bc5c7458be516c
J-Kehoe <jkkehoe177@gmail.com>
2022-01-17 12:28:44 +1100

    update bundler

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(
         mode: 'production',
         output:
         {
-            publicPath: '/puppet-show-JS'
+            publicPath: '/puppet-show-JS/'
         },
         plugins:
         [
